561d936ebf46d2a814f8cdfa0e9149eb3bcd0bb0
Gennady <topchiev@list.ru>
2023-11-12 17:27:24 +0300

    modified test_decorators.py

--- tests/test_decorators.py
+++ tests/test_decorators.py
@@ -1,19 +1,57 @@
 import pytest
+import os.path
+from datetime import datetime
 
 from src.decorators import log
 
+
 @pytest.mark.parametrize(
-    "x, y, expected",
+    "a, b, expected",
     [
-        (10, 5, 2),
-        (0, 2, 0),
-        (10, 0, None),
-        (10, '2', None)
+        (10, 5, " my_function ok"),
+        (0, 2, " my_function ok"),
+        (10, 0, " my_function error: division by zero"),
+        (10, "2", " my_function error: unsupported operand type(s) for /: 'int' and 'str'"),
     ],
 )
-def test_decorated_function(x, y, expected):
-    @log(filename="mylog.txt")
-    def my_function():
+def test_log_decorator(a, b, expected):
+    filename = "mylog.txt"
+    if os.path.exists(filename):
+        os.remove(filename)
+
+    @log(filename)
+    def my_function(x: int, y: int) -> float:
         return x / y
-    result = my_function()
-    assert result == expected
+
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    my_function(a, b)
+
+    with open(filename) as file:
+        log_message = file.read().strip()
+
+    expected_log = now + expected
+
+    assert log_message == expected_log
+
+
+@pytest.mark.parametrize(
+    "a, b, expected",
+    [
+        (10, 5, " my_function ok"),
+        (0, 2, " my_function ok"),
+        (10, 0, " my_function error: division by zero"),
+        (10, "2", " my_function error: unsupported operand type(s) for /: 'int' and 'str'"),
+    ],
+)
+def test_log_to_console(capsys, a, b, expected):
+    @log()
+    def my_function(x: int, y: int) -> float:
+        return x / y
+
+    now = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    my_function(a, b)
+
+    expected_log = now + expected
+    log_message = capsys.readouterr()
+
+    assert log_message.out.strip() == expected_log
