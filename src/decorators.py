54a965148aab577bcafdf0aad3b7dc955c928354
Gennady <topchiev@list.ru>
2023-11-12 17:26:51 +0300

    modified decorators.py

--- src/decorators.py
+++ src/decorators.py
@@ -1,31 +1,35 @@
 from datetime import datetime
 from functools import wraps
-from typing import Callable
+from typing import Any, Callable, Optional
 
-def log(filename: str = None) -> Callable:
+
+def log(filename: Optional[str] = None) -> Callable:
     """
     Логирует вызов функции и ее результат в файл или в консоль. Принимает один необязательный
     аргумент filename, который определяет имя файла, в который будут записываться логи.
-    Если filename не задан, то логи будут выводиться в консоль. Если вызов функции закончился ошибкой,
-    то записывается сообщение об ошибке и входные параметры функции.
+    Если filename не задан, то логи будут выводиться в консоль. Если вызов функции закончился
+    ошибкой, то записывается сообщение об ошибке и входные параметры функции.
     :param filename:
     :return logs:
     """
-    def wrapper(func):
+
+    def wrapper(func: Callable) -> Callable:
         @wraps(func)
-        def inner(*args, **kwargs):
+        def inner(*args: Any, **kwargs: Any) -> Any:
             try:
                 result = func(*args, **kwargs)
-                print(f"{datetime.now().replace(microsecond=0)} {func.__name__} ok\n")
-                if filename:
-                    with open(filename, "a") as file:
-                        file.write(f"{datetime.now().replace(microsecond=0)} {func.__name__} ok\n")
+                log_message = f"{datetime.now().replace(microsecond=0)} {func.__name__} ok\n"
             except Exception as err:
                 result = None
-                print(f"{datetime.now().replace(microsecond=0)} {func.__name__} error: {err}\n")
-                if filename:
-                    with open(filename, "a") as file:
-                        file.write(f"{datetime.now().replace(microsecond=0)} {func.__name__} error: {err}\n")
+                log_message = (
+                    f"{datetime.now().replace(microsecond=0)} {func.__name__} error: {err}\n"
+                )
+            if filename:
+                with open(filename, "a") as file:
+                    file.write(log_message)
+            print(log_message)
             return result
+
         return inner
+
     return wrapper
