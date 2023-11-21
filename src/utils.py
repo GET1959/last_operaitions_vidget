70b5744cfe8eb971f762c411497ac1f1bf90d593
Gennady <topchiev@list.ru>
2023-11-21 16:08:19 +0300

    modified utils/py test_utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,9 +1,21 @@
 import json
 import os
+import logging
 from typing import Any
 
 import requests
 
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+fh = logging.FileHandler('utils.log', mode='w', encoding='utf-8')
+fh.setLevel(logging.INFO)
+
+formatter = logging.Formatter('%(asctime)s - %(module)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+
+logger.addHandler(fh)
 
 def get_operations(file_name: str) -> Any:
     """
@@ -17,12 +29,17 @@ def get_operations(file_name: str) -> Any:
     path_to_file = os.path.join(cur_dir + "/data/" + file_name)
     try:
         with open(path_to_file, encoding="utf-8") as file:
-            return json.load(file) + []
+            result = json.load(file) + []
+            logger.info("Получен файл с данными в формате list.")
+            return result
     except json.decoder.JSONDecodeError:
+        logger.error("Передан пустой файл.")
         return []
     except TypeError:
+        logger.error("Содержимое файла не является списком.")
         return []
     except FileNotFoundError:
+        logger.error("Файл не найден.")
         return []
 
 
@@ -37,18 +54,26 @@ def get_rub_sum(transaction: dict, currency: str = "RUB") -> Any:
     :return sum of transaction:
     """
     if transaction["operationAmount"]["currency"]["code"] == currency:
+        logger.info(f"Выведена транзакция, проведенная в {currency}.")
         return float(transaction["operationAmount"]["amount"])
-    raise ValueError("Транзация выполнена не в рублях. Укажите транзакцию в рублях")
+    logger.error(f"Транзация выполнена не в {currency}")
+    raise ValueError(f"Транзация выполнена не в {currency}")
 
 
-def get_amount_in_rub(transaction: dict) -> Any:
+def get_amount_in_rub(file_name: str) -> Any:
     """
     Функция принимает на вход одну транзакцию
     и возвращает сумму транзакции (amount) в рублях, тип float
-    :param transaction:
+    :param file_name:
     :return sum of transaction:
     """
     url = "https://www.cbr-xml-daily.ru/daily_json.js"
+
+    cur_dir = os.path.dirname(os.path.abspath("."))
+    path_to_file = os.path.join(cur_dir + "/data/" + file_name)
+
+    with open(path_to_file, encoding="utf-8") as file:
+        transaction = json.load(file)
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
@@ -56,8 +81,18 @@ def get_amount_in_rub(transaction: dict) -> Any:
         cur = transaction["operationAmount"]["currency"]["code"]
         amount = transaction["operationAmount"]["amount"]
         if cur == "RUB":
+            logger.info("Выведена сумма транзакции, проведенной в рублях.")
             return float(amount)
         else:
+            logger.info(f"Выведен рублевый эквивалент транзакции, проведенной в {cur}.")
             return round(float(amount) * data_dict[cur], 2)
     else:
+        logger.error(f"Failed to receive data. Status code: {response.status_code}")
         return None
+
+
+print(get_amount_in_rub("trans_2.json"))
+#print(get_operations('list_file.json'))
+#print(get_operations('some_file.json'))
+#print(get_operations('empty_file.json'))
+#assert get_amount_in_rub('trans_2.json') == 722412.6
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,26 +1,28 @@
+import os
+import json
 import pytest
+from unittest.mock import Mock, patch
 import requests
 
-from src.utils import get_operations, get_rub_sum
+from src.utils import get_operations, get_rub_sum, get_amount_in_rub
 
-URL = 'https://jsonkeeper.com/b/DRYA'  # operations.json
+#URL = 'https://jsonkeeper.com/b/DRYA'  # operations.json
 
-
+cur_dir = os.path.dirname(os.path.abspath('.'))
+path_to_file = os.path.join(cur_dir + '/data/')
 @pytest.fixture()
 def list_of_transactions():
-    data = requests.get(URL, verify=False).json()
-    return data
+    with open(path_to_file + "operations.json", encoding='utf-8') as file:
+        return json.load(file)
 
 
-@pytest.mark.parametrize(
-    "file, expected",
-    [
-        ('operations.json', requests.get(URL, verify=False).json()),
+@pytest.mark.parametrize("file, expected",
+    [('list_file.json', [{"id": 441945886, "state": "EXECUTED", "date": "2019-08-26T10:50:58.294041"},
+     {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+     {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"}]),
         ("dict_file.json", []),
         ("empty_file.json", []),
-        ("some_file.json", []),
-    ],
-)
+        ("some_file.json", []),])
 def test_get_operations(file, expected):
     assert get_operations(file) == expected
 
@@ -33,3 +35,23 @@ def test_get_rub_sum(list_of_transactions, currency, expected):
 def test_get_rub_sum_usd(list_of_transactions):
     with pytest.raises(ValueError):
         get_rub_sum(list_of_transactions[1], "RUB")
+
+
+
+#path_to_file = os.path.join('C:/Users/Gennady/Skypro_learning/last_operaitions_vidget/data/operations.json')
+
+@patch('requests.get')
+#@patch('builtins.open', create=True)
+def test_get_amount_in_rub(mock_get):
+    #mock_file = mock_open.return_value
+    #mock_file.read.return_value = 'USD'
+    mock_get.return_value.json.return_value = 722412.6
+    assert get_amount_in_rub('trans_2.json') == 722412.6
+    #mock_open.assert_called_once_with(path_to_file + 'trans_2.json', 'r', encoding='utf-8')
+    mock_get.assert_called_once_with("https://www.cbr-xml-daily.ru/daily_json.js")
+
+# @patch('requests.get')
+# def test_get_github_user_info(mock_get):
+#     mock_get.return_value.json.return_value = {'login': 'testuser', 'name': 'Test User'}
+#     assert get_github_user_info('testuser') == {'login': 'testuser', 'name': 'Test User'}
+#     mock_get.assert_called_once_with('https://api.github.com/users/testuser')
