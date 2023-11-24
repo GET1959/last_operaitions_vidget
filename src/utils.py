5fd443243f3fb255781efdc67f699bff784f611a
Gennady <topchiev@list.ru>
2023-11-24 21:30:14 +0300

    modified utils test_utils

--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,22 @@
 import json
+import logging
 import os
 from typing import Any
 
+import pandas as pd
 import requests
+
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+fh = logging.FileHandler("utils.log", mode="w", encoding="utf-8")
+fh.setLevel(logging.INFO)
+
+formatter = logging.Formatter("%(asctime)s - %(module)s - %(levelname)s - %(message)s")
+fh.setFormatter(formatter)
+
+logger.addHandler(fh)
 
 
 def get_operations(file_name: str) -> Any:
@@ -17,12 +31,17 @@ def get_operations(file_name: str) -> Any:
     path_to_file = os.path.join(cur_dir + "/data/" + file_name)
     try:
         with open(path_to_file, encoding="utf-8") as file:
-            return json.load(file) + []
+            result = json.load(file) + []
+            logger.info("Получен файл с данными в формате list.")
+            return result
     except json.decoder.JSONDecodeError:
+        logger.error("Передан пустой файл.")
         return []
     except TypeError:
+        logger.error("Содержимое файла не является списком.")
         return []
     except FileNotFoundError:
+        logger.error("Файл не найден.")
         return []
 
 
@@ -37,18 +56,26 @@ def get_rub_sum(transaction: dict, currency: str = "RUB") -> Any:
     :return sum of transaction:
     """
     if transaction["operationAmount"]["currency"]["code"] == currency:
+        logger.info(f"Выведена транзакция, проведенная в {currency}.")
         return float(transaction["operationAmount"]["amount"])
-    raise ValueError("Транзация выполнена не в рублях. Укажите транзакцию в рублях")
+    logger.error(f"Транзация выполнена не в {currency}")
+    raise ValueError(f"Транзация выполнена не в {currency}")
 
 
-def get_amount_in_rub(transaction: dict) -> Any:
+def get_amount_in_rub(file_name: str) -> Any:
     """
     Функция принимает на вход одну транзакцию
     и возвращает сумму транзакции (amount) в рублях, тип float
-    :param transaction:
+    :param file_name:
     :return sum of transaction:
     """
     url = "https://www.cbr-xml-daily.ru/daily_json.js"
+
+    cur_dir = os.path.dirname(os.path.abspath("."))
+    path_to_file = os.path.join(cur_dir + "/data/" + file_name)
+
+    with open(path_to_file, encoding="utf-8") as file:
+        transaction = json.load(file)
     response = requests.get(url)
     if response.status_code == 200:
         data = response.json()
@@ -56,8 +83,50 @@ def get_amount_in_rub(transaction: dict) -> Any:
         cur = transaction["operationAmount"]["currency"]["code"]
         amount = transaction["operationAmount"]["amount"]
         if cur == "RUB":
+            logger.info("Выведена сумма транзакции, проведенной в рублях.")
             return float(amount)
         else:
+            logger.info(f"Выведен рублевый эквивалент транзакции, проведенной в {cur}.")
             return round(float(amount) * data_dict[cur], 2)
     else:
+        logger.error(f"Failed to receive data. Status code: {response.status_code}")
         return None
+
+
+def table_to_dict_list(file: str) -> Any:
+    """
+    Принимает на вход таблицу трансакций в формате csv или excel и
+    возвращает список словарей, отражающих трансакцию в формате
+    настоящего проекта.
+    :param file:
+    :return Any:
+    """
+    cur_dir = os.path.dirname(os.path.abspath("."))
+    path_to_file = os.path.join(cur_dir + "/data/" + file)
+    ext = os.path.splitext(path_to_file)[1]
+    if ext == '.csv':
+        df = pd.read_csv(path_to_file, delimiter=';', encoding='utf-8')
+
+    elif ext == '.xls' or ext == '.xlsx':
+        df = pd.read_excel(path_to_file)
+
+    else:
+        logger.error("Файл с таким расширением не поддерживается")
+        return None
+
+    df = df.fillna('unknown_source')
+    df = df.rename(columns={'currency_name': 'name', 'currency_code': 'code'})
+
+    currency = df.loc[:, ['name', 'code']].to_dict(orient='records')
+    operationAmount = df.loc[:, ['amount']].to_dict(orient='records')
+    operationAmount = [{'operationAmount': {'amount': am['amount'], 'currency': cur}}
+                       for am, cur in zip(operationAmount, currency)]
+
+    dict_1 = df.loc[:, ['id', 'state', 'date']].to_dict(orient='records')
+    dict_2 = df.loc[:, ['from', 'to', 'description']].to_dict(orient='records')
+
+    trans_list = [dict_1[i] | operationAmount[i] | dict_2[i] for i in range(len(df))]
+
+    logger.info(f"Успешно получены данные из файла {ext}.")
+
+    return trans_list
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,9 +1,10 @@
 import pytest
 import requests
 
-from src.utils import get_operations, get_rub_sum
+from src.utils import get_operations, get_rub_sum, table_to_dict_list
 
-URL = 'https://jsonkeeper.com/b/DRYA'  # operations.json
+
+URL = "https://jsonkeeper.com/b/DRYA"  # operations.json
 
 
 @pytest.fixture()
@@ -15,7 +16,7 @@ def list_of_transactions():
 @pytest.mark.parametrize(
     "file, expected",
     [
-        ('operations.json', requests.get(URL, verify=False).json()),
+        ("operations.json", requests.get(URL, verify=False).json()),
         ("dict_file.json", []),
         ("empty_file.json", []),
         ("some_file.json", []),
@@ -33,3 +34,95 @@ def test_get_rub_sum(list_of_transactions, currency, expected):
 def test_get_rub_sum_usd(list_of_transactions):
     with pytest.raises(ValueError):
         get_rub_sum(list_of_transactions[1], "RUB")
+
+
+@pytest.mark.parametrize(
+    "file_name, expected",
+    [
+        (
+            "test.csv",
+            [
+                {
+                    "id": 0,
+                    "state": "EXECUTED",
+                    "date": "2023-09-05",
+                    "operationAmount": {
+                        "amount": 50000,
+                        "currency": {"name": "rub", "code": "RUB"},
+                    },
+                    "from": "user1",
+                    "to": "user2",
+                    "description": "перевод",
+                },
+                {
+                    "id": 1,
+                    "state": "EXECUTED",
+                    "date": "2023-11-03",
+                    "operationAmount": {
+                        "amount": 70000,
+                        "currency": {"name": "usd", "code": "USD"},
+                    },
+                    "from": "user2",
+                    "to": "user3",
+                    "description": "кредит",
+                },
+                {
+                    "id": 2,
+                    "state": "CANCELED",
+                    "date": "2023-11-10",
+                    "operationAmount": {
+                        "amount": 20000,
+                        "currency": {"name": "eur", "code": "EUR"},
+                    },
+                    "from": "user4",
+                    "to": "user5",
+                    "description": "операция не подтверждена",
+                },
+            ],
+        ),
+        (
+            "test.xlsx",
+            [
+                {
+                    "id": 0,
+                    "state": "EXECUTED",
+                    "date": "2023-10-01",
+                    "operationAmount": {
+                        "amount": 40000,
+                        "currency": {"name": "rub", "code": "RUB"},
+                    },
+                    "from": "user0",
+                    "to": "user1",
+                    "description": "перевод",
+                },
+                {
+                    "id": 1,
+                    "state": "CANCELED",
+                    "date": "2023-09-05",
+                    "operationAmount": {
+                        "amount": 20000,
+                        "currency": {"name": "aud", "code": "AUD"},
+                    },
+                    "from": "user5",
+                    "to": "user2",
+                    "description": "операция не подтверждена",
+                },
+                {
+                    "id": 2,
+                    "state": "EXECUTED",
+                    "date": "2023-11-07",
+                    "operationAmount": {
+                        "amount": 80000,
+                        "currency": {"name": "usd", "code": "USD"},
+                    },
+                    "from": "user7",
+                    "to": "user3",
+                    "description": "погашение",
+                },
+            ],
+        ),
+        ("trans_3.json", None),
+    ],
+)
+def test_table_to_dict_list(file_name, expected):
+    assert table_to_dict_list(file_name) == expected
