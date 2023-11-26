bc743fb98ca5c6a89cb17a4fd7c0663c99d6ff10
Gennady <topchiev@list.ru>
2023-11-26 23:36:40 +0300

    modified test_re_utils

--- tests/test_re_utils.py
+++ tests/test_re_utils.py
@@ -1,21 +1,38 @@
+from src.re_utils import category_count, find_operation
 from src.utils import table_to_dict_list
 
-from src.re_utils import find_operation, category_count
 
-
-transact_list = table_to_dict_list("transactions_excel.xlsx")
+TRANS_LIST = table_to_dict_list("transactions_excel.xlsx")
 
 
 def test_find_operation():
-    assert find_operation(transact_list, "Открытие") == [
-        trans for trans in transact_list if "Открытие" in trans["description"]
+    assert find_operation(TRANS_LIST, "Открытие") == [
+        trans for trans in TRANS_LIST if "Открытие" in trans["description"]
     ]
 
 
+CAT_DICT = {
+    "Перевод организации": 0,
+    "Перевод с карты на карту": 0,
+    "Открытие вклада": 0,
+    "Перевод со счета на счет": 0,
+}
+
+
 def test_category_count():
-    assert category_count(transact_list) == {
+    assert category_count(TRANS_LIST, CAT_DICT) == {
         "Перевод организации": 117,
         "Перевод с карты на карту": 587,
+        "Открытие вклада": 185,
+        "Перевод со счета на счет": 110,
+    }
+    assert category_count(
+        TRANS_LIST,
+        {
+            "Открытие вклада": 0,
+            "Перевод со счета на счет": 0,
+        },
+    ) == {
         "Открытие вклада": 185,
         "Перевод со счета на счет": 110,
     }
